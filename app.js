40878d9faf4a1a91b46911db61cb8cf13a614133
steve yunker <yunkers@gmatc.matc.edu>
2022-03-06 11:55:32 -0600

    completed project 1

--- app.js
+++ app.js
@@ -1,69 +1,92 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const app = express();
-app.use(bodyParser.json());
 const path = require('path');
 
 const db = require("./db");
 const collection = "todo";
+const app = express();
 
+
+// parses json data sent to us by the user 
+app.use(bodyParser.json());
+
+// serve static html file to user
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 });
 
+// read and get all todo messages
 app.get('/getTodos',(req,res)=>{
     db.getDB().collection(collection).find({}).toArray((err,documents)=>{
         if(err)
             console.log(err);
         else{
-            console.log(documents);
             res.json(documents);
         }
     });
 });
 
+// update
 app.put('/:id',(req,res)=>{
     const todoID = req.params.id;
     const userInput = req.body;
-
-    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)}, {$set : {todo : userInput.todo}}, {returnOriginal : false},(err,result)=>{
+    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(todoID)},{$set : {todo : userInput.todo}},{returnOriginal : false},(err,result)=>{
         if(err)
             console.log(err);
         else{
             res.json(result);
-        }
-    })
+        }      
+    });
 });
 
+
+//create
 app.post('/',(req,res)=>{
     const userInput = req.body;
     db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
         if(err)
-            console.log(err);
-        else
+          console.log(err);
+        else {
             res.json({result : result, document : result.ops[0]});
-    });
-});
+        }
+            });
+        });
+    
 
+
+
+//delete
 app.delete('/:id',(req,res)=>{
     const todoID = req.params.id;
     db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
         if(err)
             console.log(err);
-        else{
+        else
             res.json(result);
+    });
+});
+
+// Middleware for handling Error
+app.use((err,req,res,next)=>{
+    res.status(404).json({
+        error : {
+            message : err.message
         }
     });
 });
 
+
 db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
     if(err){
         console.log('unable to connect to database');
         process.exit(1);
     }
+    // Successfully connected to database
     else{
         app.listen(3000,()=>{
-            console.log('connected to database, app listening on port 3000')
+            console.log('connected to database, app listening on port 3000');
         });
     }
 });
